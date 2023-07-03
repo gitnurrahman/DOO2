307c4c7ac5a3fecc1720f2f2a1e87387cf1f3855
gitnurrahman <m360dkx3793@bangkit.academy>
2023-07-03 13:52:44 +0700

    'new'

--- Code/paling_mantap/paling_mantap.ino
+++ Code/paling_mantap/paling_mantap.ino
@@ -11,14 +11,14 @@ unsigned char pinFlowsensor = 2;
 
 AccelStepper stepper(1, stepPin, dirPin);
 int step_valve;
-
+int step_valve1;
 const int numReadings = 5;
 int readings[numReadings];
 int index = 0;
 double total = 0;
 double average = 0;
 double duration, distance;
-double tinggi = 0;
+double tinggi1 = 0;
 
 int readings2[numReadings];
 int index2 = 0;
@@ -37,9 +37,17 @@ double dt;
 double integral, error_sebelumnya, output_PID = 0;
 double last_time = 0;
 double setPoint ;
-double kp ;
-double ki ;
-double kd ;
+double Kp ;
+double Ki ;
+double Kd ;
+unsigned long Millis_sebelumnya = 0;
+double errorSum1 = 0.00;
+double lastError1 = 0.00;
+double outputPID1 = 0.00;        //output PID tank 1
+double errorSum2 = 0.00;
+double lastError2 = 0.00;
+double outputPID2 = 0.00;        //output PID tank 1
+int tangki = 0;
 String input;
 String separator = ",";
 void cacahPulsa()
@@ -47,7 +55,6 @@ void cacahPulsa()
   pulsa_sensor++;
 }
 
-double pid(double error);
 
 void setup()
 {
@@ -81,6 +88,7 @@ void setup()
   stepper.setMaxSpeed(1000);
   stepper.setAcceleration(500);
   step_valve = 0;
+  step_valve1 = 0;
 }
 
 void loop()
@@ -101,8 +109,8 @@ void loop()
     index = 0;
   }
   average = total / 5;
-  tinggi = 48 - average;
-  constrain(tinggi, 0, 48);
+  tinggi1 = 48 - average;
+  constrain(tinggi1, 0, 48);
 
   digitalWrite(triggerPin2, LOW);
   delayMicroseconds(2);
@@ -121,7 +129,7 @@ void loop()
   }
   average2 = total2 / 5;
   tinggi2 = 33 - average2;
-  constrain(tinggi, 0, 33);
+  constrain(tinggi2, 0, 33);
 
   waktuAktual = millis();
   if (waktuAktual >= (waktuLoop + 1000))
@@ -133,7 +141,7 @@ void loop()
   }
 
   //  SEND SENSOR DATA
-  String Data = String(tinggi) + "," + String (tinggi2) + "," + String(literPermenit) + ","  + String(step_valve);
+  String Data = String(tinggi1) + "," + String (tinggi2) + "," + String(literPermenit) + ","  + String(step_valve);
   Serial2.println(Data);
   delay(100);
   // ON OFF Pump
@@ -147,14 +155,11 @@ void loop()
     else if (command == '0')
     { //OFF
       digitalWrite(relayPin, HIGH);
-      stepper.moveTo(0);
-      stepper.runToPosition();
+      tangki = 0;
     }
     else if (command == '4')
-    { step_valve += 800;
-    }
-    else if (command == '5')
-    { step_valve -= 800;
+    { //OFF
+      tangki = 3;
     }
   }
 
@@ -162,10 +167,10 @@ void loop()
   {
     char command = Serial1.read();
     if (command == '2') {
-      tangki_1();
+      tangki = 1;
     }
     else if (command == '3') {
-      tangki_2();
+      tangki = 2;
     }
   }
 
@@ -173,11 +178,49 @@ void loop()
   { input = Serial2.readStringUntil('\n');
     // Memanggil fungsi pemisahan nilai
     splitValues(input);
-    Serial.print(kp);
-    Serial.print(ki);
-    Serial.print(kd);
+    Serial.print(Kp);
+    Serial.print(Ki);
+    Serial.print(Kd);
     Serial.print(setPoint);
-    Serial.print(step_valve);
+    Serial.print(step_valve1);
+  }
+  if (tangki == 1) {
+    computePID(setPoint, tinggi1, errorSum1, lastError1, outputPID1);
+    float x = outputPID1;         //output PID yg berupa flow ke besar step valve
+    float y = (-70) * x + 5400;
+    step_valve = y;
+    if (step_valve > 5300) {
+      step_valve = 5300;
+    }
+    else {
+      step_valve = step_valve;
+    }
+
+    stepper.moveTo(step_valve);   //PID gerakkan vlave
+    stepper.runToPosition();
+  }
+  else if (tangki == 2) {
+    computePID(setPoint, tinggi2, errorSum2, lastError2, outputPID2);
+    float c = outputPID1;         //output PID yg berupa flow ke besar step valve
+    float d = (-70) * c + 5400;
+    step_valve = d;
+    if (step_valve > 5300) {
+      step_valve = 5300;
+    }
+    else {
+      step_valve = step_valve;
+    }
+
+    stepper.moveTo(step_valve);   //PID gerakkan vlave
+    stepper.runToPosition();
+  }
+  else if (tangki == 3) {
+    stepper.moveTo(step_valve1);          //Menutup vlave
+    stepper.runToPosition();
+  }
+  else if (tangki == 0) {
+    stepper.moveTo(100);          //Menutup vlave
+    stepper.runToPosition();
   }
 }
 void splitValues(String input) {
@@ -195,55 +238,32 @@ void splitValues(String input) {
   String valveString = input.substring(separatorIndex4 + 1);
 
   // Mengkonversi nilai ke tipe double dan int
-  kp = kpString.toDouble();
-  ki = kiString.toDouble();
-  kd = kdString.toDouble();
+  Kp = kpString.toDouble();
+  Ki = kiString.toDouble();
+  Kd = kdString.toDouble();
   setPoint = setPointString.toDouble();
-  step_valve = valveString.toInt();
+  step_valve1 = valveString.toInt();
 }
-double pid(double error)
-{
-  double proportional = error;
-  integral += error * dt;
-  double derivative = (error - error_sebelumnya) / dt;
-  error_sebelumnya = error;
-  double output_PID = (kp * proportional) + (ki * integral) + (kd * derivative);
-  if (output_PID < -15) {
-    output_PID = -15;
+void computePID(double setPoint, double tinggi, double& errorSum, double& lastError, double& outputPID) {
+  // Compute time difference
+  unsigned long Millis_sekarang = millis();
+  double deltaTime = (Millis_sekarang - Millis_sebelumnya) / 1000.00;
+
+  // Compute error and PID output
+  double error = setPoint - tinggi;
+  errorSum += error * deltaTime;
+  double dError = (error - lastError) / deltaTime;
+  outputPID = (Kp * error) + (Ki * errorSum) + (Kd * dError);
+  if (outputPID > 15) {
+    outputPID = 15;
   }
-  else if (output_PID > 15) {
-    output_PID = 15;
-  } return output_PID;
-}
-void tangki_1() {
-  for (;;) {
-    double now = millis();
-    dt = (now - last_time) / 1000.00;
-    last_time = now;
-    double error = setPoint - tinggi;
-    output_PID = pid(error);
-
-    float x = output_PID;
-    float y = (-70) * x + 5400;
-    step_valve = constrain(y, 0, 5200);
-    stepper.moveTo(step_valve);
-    stepper.runToPosition();
+  else if (outputPID < -15) {
+    outputPID = -15;
   }
-}
-
-void tangki_2() {
-  for (;;) {
-    double now = millis();
-    dt = (now - last_time) / 1000.00;
-    last_time = now;
-    double error = setPoint - tinggi2;
-    output_PID = pid(error);
-
-    float x = output_PID;
-    float y = (-70) * x + 5400;
-    step_valve = constrain(y, 0, 5200);
-    stepper.moveTo(step_valve);
-    stepper.runToPosition();
+  else {
+    outputPID = outputPID;
   }
 
+  Millis_sebelumnya = Millis_sekarang;
+  lastError = error;
 }
