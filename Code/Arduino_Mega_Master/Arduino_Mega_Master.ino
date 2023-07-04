25ecc554a3ecadf9695b9102b367bf517a082d3e
gitnurrahman <m360dkx3793@bangkit.academy>
2023-07-04 21:55:47 +0700

    'new'

--- Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
+++ Code/Arduino_Mega_Master/Arduino_Mega_Master.ino
@@ -1,5 +1,5 @@
+double set = 0;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial2.begin(9600);
   Serial3.begin(9600);
@@ -7,28 +7,39 @@ void setup() {
 
 void loop() {
   // PUMP
-  if (Serial.available ()>0) {
-    // read user input from computer
-    char command = Serial.read();
-    if (command == '1')
-    { //ON
-      Serial3.println(command); 
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+
+    if (command == "1") {
+      //       ON
+      Serial3.println('1');
     }
-    else if (command == '0')
-    { //OFF
-      Serial3.println(command); 
+    else if (command == "0") {
+      //       OFF
+      Serial3.println('0');
     }
-    else if (command == 'P')
-    { //PID
-      String input = Serial.readStringUntil('\n');
-      Serial1.println(input); 
+    else if (command == "2") {
+      //         Tangki 1
+      Serial3.println('2');
     }
+    else if (command == "3") {
+      //      Tangki 2
+      Serial3.println('3');
     }
-    
-//SENSOR
-if (Serial2.available()>0) {
-    String Data = Serial2.readStringUntil('\n');
-    Serial.println(Data);
+    else if (command == "M") {
+      //      Manual
+      Serial3.println('4');
+    }
+    else if (command.startsWith("P")) {
+      //      Setpoint
+      Serial2.println(command);
+
+    }
   }
-  
+
+  // SENSOR
+  if (Serial2.available() > 0) {
+    String data = Serial2.readStringUntil('\n');
+    Serial.println(data);
+  }
 }
